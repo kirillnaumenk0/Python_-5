7efee50b611d354478c72f5f31ed224068d57924
Kirill <kirillnaumenk0@mail.ru>
2022-10-13 15:56:57 +0600

    commit 1

--- №1.py
+++ №1.py
@@ -18,3 +18,27 @@
 
 # out
 # The data is incorrect
+
+
+from random import sample
+
+
+def list_words(a: int, b: str = 'абв'):
+    my_list = []
+    for i in range(a):
+        enter = sample(b, 3)
+        my_list.append("".join(enter))
+    return " ".join(my_list)
+
+
+def second_list_words(words: str):
+    return " ".join(i for i in words.split() if i != "абв")
+
+
+word = int(input("Число слов: "))
+if word <= 0:
+    print('The data is incorrect')
+else:
+    end_list = list_words(word)
+    print(end_list)
+    print(second_list_words(end_list))
