3f35023668c51564ecd07855dc9f0ecc74a0823c
Kirill <kirillnaumenk0@mail.ru>
2022-10-13 16:36:33 +0600

    commit end

--- №2.py
+++ №2.py
@@ -1,23 +1,7 @@
-# 2. Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.Входные и выходные данные хранятся в отдельных текстовых файлах.
-# Алгоритм RLE
+# 2. Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
+#    Входные и выходные данные хранятся в отдельных текстовых файлах.
 
-# in
-# Enter the name of the file with the text:
-# 'text_words.txt'
-# Enter the file name to record:
-# 'text_code_words.txt'
-# Enter the name of the file to decode:
-# 'text_code_words.txt'
-
-# out
-# aaaaavvvvvvvvvvvvvvvvvvvvvvvvvvvvvssssDDDdddFFggggOOiiiaa
-# vvvvvvvvvvvbbwwPPuuuTTYyWWQQ
-
-# out in file
-# 'text_words.txt'
-# aaaaavvvvvvvvvvvvvvvvvvvvvvvvvvvvvssssDDDdddFFggggOOiiiaa
-# vbbwwPPuuuTTYyWWQQ
-
-# 'text_code_words.txt'
-# 5a29v4s3D3d2F4g2O3i2a1
-# 1v2b2w2P3u2T1Y1y2W2Q
+# не понял вообще ничего, + нехватка времени, меня очень фрустрировала эта задачка, даже не знаю как быть
+# видимо фалы это не мой уровень, как то странно, обещали что python будет проще, хотя С# мне был намного интуитивнее понятен,
+# а здесь непонятные встроенные функции и т.д
+# такое ощущение что я отстал от своих одногрупников, либо я единственный с 0 базовым знанием сюда пришел
